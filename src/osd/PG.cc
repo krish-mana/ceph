2d742371ba0a2ba3791c9cd1e25d538e770a70af
Samuel Just <samuel.just@dreamhost.com>
2012-04-18 15:20:02 -0700

    OSD,PG: push message checking to pg
    old_peering_evt now checks CephPeeringEvts generically in
    PG::handle_peering_event().
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -3789,13 +3789,21 @@ ostream& operator<<(ostream& out, const PG& pg)
   return out;
 }
 
+void PG::handle_peering_event(CephPeeringEvtRef evt, RecoveryCtx *rctx)
+{
+  if (old_peering_evt(evt))
+    return;
+  assert(!deleting);
+  recovery_state.handle_event(evt, rctx);
+}
+
 void PG::handle_notify(epoch_t msg_epoch,
 		       epoch_t query_epoch,
 		       int from, pg_info_t& i,
 		       RecoveryCtx *rctx)
 {
   dout(10) << "handle_notify " << i << " from osd." << from << dendl;
-  recovery_state.handle_event(
+  handle_peering_event(
     CephPeeringEvtRef(new CephPeeringEvt(msg_epoch, query_epoch,
 					 new MNotifyRec(from, i))),
     rctx);
@@ -3807,7 +3815,7 @@ void PG::handle_info(epoch_t msg_epoch,
 		     RecoveryCtx *rctx)
 {
   dout(10) << "handle_info " << i << " from osd." << from << dendl;
-  recovery_state.handle_event(
+  handle_peering_event(
     CephPeeringEvtRef(new CephPeeringEvt(msg_epoch, query_epoch,
 					 new MInfoRec(from, i))),
     rctx);
@@ -3820,7 +3828,7 @@ void PG::handle_log(epoch_t msg_epoch,
 		    RecoveryCtx *rctx)
 {
   dout(10) << "handle_log " << *msg << " from osd." << from << dendl;
-  recovery_state.handle_event(
+  handle_peering_event(
     CephPeeringEvtRef(new CephPeeringEvt(msg_epoch, query_epoch,
 					 new MLogRec(from, msg))),
     rctx);
@@ -3832,7 +3840,7 @@ void PG::handle_query(epoch_t msg_epoch,
 		      RecoveryCtx *rctx)
 {
   dout(10) << "handle_query " << q << " from osd." << from << dendl;
-  recovery_state.handle_event(
+  handle_peering_event(
     CephPeeringEvtRef(new CephPeeringEvt(msg_epoch, query_epoch,
 					 new MQuery(from, q, query_epoch))),
     rctx);
