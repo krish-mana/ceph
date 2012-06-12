d1f54ed7b5526b8d1b234a2fc607e1e8b5b28696
Samuel Just <sam.just@inktank.com>
2012-06-11 17:05:40 -0700

    PG,OSD: fix op checking in pg, take_waiters during ActMap
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -1442,10 +1442,10 @@ void PG::do_request(OpRequestRef op)
 {
   // do any pending flush
   do_pending_flush();
-  if (can_discard_request(op)) {
-    return;
-  } else if (must_delay_request(op)) {
+  if (must_delay_request(op)) {
     op_waiters.push_back(op);
+    return;
+  } else if (can_discard_request(op)) {
     return;
   } else if (!is_active()) {
     waiting_for_active.push_back(op);
@@ -1454,6 +1454,8 @@ void PG::do_request(OpRequestRef op)
     waiting_for_active.push_back(op);
     return;
   }
+
+  assert(!waiting_for_active.size());
 
   switch (op->request->get_type()) {
   case CEPH_MSG_OSD_OP:
@@ -3885,9 +3887,11 @@ bool PG::can_discard_op(OpRequestRef op)
   } else if (m->may_write() &&
 	     (!is_primary() ||
 	      !same_for_modify_since(m->get_map_epoch()))) {
+    osd->handle_misdirected_op(this, op);
     return true;
   } else if (m->may_read() &&
 	     !same_for_read_since(m->get_map_epoch())) {
+    osd->handle_misdirected_op(this, op);
     return true;
   } else if (is_replay()) {
     if (m->get_version().version > 0) {
@@ -3990,8 +3994,12 @@ bool PG::must_delay_request(OpRequestRef op)
 
 void PG::queue_op(OpRequestRef op)
 {
-  if (can_discard_request(op))
+  if (must_delay_request(op)) {
+    op_waiters.push_back(op);
     return;
+  } else if (can_discard_request(op)) {
+    return;
+  }
   op_queue.push_back(op);
   osd->queue_for_op(this);
 }
@@ -4337,6 +4345,7 @@ boost::statechart::result PG::RecoveryState::Primary::react(const ActMap&)
   dout(7) << "handle ActMap primary" << dendl;
   PG *pg = context< RecoveryMachine >().pg;
   pg->update_stats();
+  pg->take_waiters();
   return discard_event();
 }
 
@@ -4696,6 +4705,7 @@ boost::statechart::result PG::RecoveryState::ReplicaActive::react(const ActMap&)
 							 pg->info),
 					     pg->past_intervals);
   }
+  pg->take_waiters();
   return discard_event();
 }
 
@@ -4807,6 +4817,7 @@ boost::statechart::result PG::RecoveryState::Stray::react(const ActMap&)
 							 pg->info),
 					     pg->past_intervals);
   }
+  pg->take_waiters();
   return discard_event();
 }
 
