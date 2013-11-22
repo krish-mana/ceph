c45eaaa16dee9ccbfb53789ab0852496a12a778a
Samuel Just <sam.just@inktank.com>
2013-11-22 11:20:23 -0800

    OSDMonitor: add debug_fake_ec_pool
    This flag will cause ReplicatedPG to act as though the
    pool were actually an EC pool in that operations will
    be restricted to operations which can be locally rolled
    back thereby allowing us to test the ReplicatedPG local
    log rollback mechanisms independent of EC.  It will also
    cause ReplicatedPG to use the async read mechanism on
    the PGBackend implementation once it is implemented.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -2853,6 +2853,11 @@ int OSDMonitor::prepare_command_pool_set(map<string,cmd_vartype> &cmdmap,
       return -EINVAL;
     }
     ss << " pool " << pool << " flag hashpspool";
+  } else if (var == "debug_fake_ec_pool") {
+    if (val == "true" || (interr.empty() && n == 1)) {
+      p.flags |= pg_pool_t::FLAG_DEBUG_FAKE_EC_POOL;
+    }
+    ss << " pool " << pool << " set debug_fake_ec_pool";
   } else {
     ss << "unrecognized variable '" << var << "'";
     return -EINVAL;
