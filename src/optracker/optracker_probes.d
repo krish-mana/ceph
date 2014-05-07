60b675878b9b99d775c614ba7bf9aeecb13da8a8
Samuel Just <sam.just@inktank.com>
2014-05-07 14:17:37 -0700

    working

--- src/optracker/optracker_probes.d
+++ src/optracker/optracker_probes.d
@@ -1,13 +1,13 @@
+struct res_t {
+   string type_id;
+   string class_id;
+   string inst_id;
+};
+struct op_t {
+   string class_id;
+   string inst_id;
+};
 provider ceph_optracker {
-  struct res_t {
-     string type_id;
-     string class_id;
-     string inst_id;
-  };
-  struct op_t {
-     string class_id;
-     string inst_id;
-  };
   probe op_event(string, string, string);
   probe res_event(struct res_t res, struct op_t op, string event, string status);
 }
